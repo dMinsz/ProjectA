c662dfb73bb8b199cd31c9733897fef6fa33eff3
Eo jin <ddw4500@naver.com>
2023-07-28 21:31:59 +0900

    feat: attack range updating

--- Assets/Game/EoJin/Scripts/Player/PlayerAttacker.cs
+++ Assets/Game/EoJin/Scripts/Player/PlayerAttacker.cs
@@ -3,27 +3,36 @@ using System.Collections.Generic;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
 
 public class PlayerAttacker : MonoBehaviour
 {
     Skill skill;
+    DataManager data;
 
+    float range;
+    float angle;
+
+    public void Awake()
+    {
+        data = GameObject.FindWithTag("DataManager").GetComponent<DataManager>();
+    }
 
     public void OnPrimarySkill(InputValue value)
     {
-        skill = gameObject.GetComponent<Character>().primarySkill;
+        skill = data.CurCharacter.primarySkill;
         ApplyDamage(skill);
     }
 
     public void OnSecondarySkill (InputValue value)
     {
-        skill = gameObject.GetComponent<Character>().secondarySkill;
+        skill = data.CurCharacter.secondarySkill;
         ApplyDamage(skill);
     }
 
     public void OnSpecailSkill(InputValue value)
     {
-        skill = gameObject.GetComponent<Character>().specialSkill;
+        skill = data.CurCharacter.specialSkill;
         ApplyDamage(skill);
     }
 
@@ -36,13 +45,13 @@ public class PlayerAttacker : MonoBehaviour
         else
             angle = 15;
 
-        Collider[] colliders = Physics.OverlapSphere(transform.position, angle);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, skill.rangeAmount);
 
         foreach (Collider collider in colliders)
         {
             Vector3 dirTarget = (collider.transform.position - transform.position).normalized;
 
-            if (!(collider.tag == "Player"))
+            if (collider.tag != "Player")
                 continue;
 
             if (Vector3.Dot(transform.forward, dirTarget) < Mathf.Cos(angle * Mathf.Deg2Rad))
@@ -51,5 +60,15 @@ public class PlayerAttacker : MonoBehaviour
             Debug.Log(collider.gameObject.name);
 
         }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+
+        Vector3 rightDir = AngleToDir(transform.eulerAngles.y + angle * 0.5f);
+        Vector3 leftDir = AngleToDir(transform.eulerAngles.y - angle * 0.5f);
+        Debug.DrawRay(transform.position, rightDir * range, Color.blue);
+        Debug.DrawRay(transform.position, leftDir * range, Color.blue);
     }
 }
