88cc7b8ed8c473fb061698a99d79284d21652d70
경빈 박 <gangbie1123@gmail.com>
2023-07-28 21:31:24 +0900

    feat: 플레이어 마우스 방향으로 공날리기 구현

--- Assets/Game/anstjddn/PlayerAim.cs
+++ Assets/Game/anstjddn/PlayerAim.cs
@@ -1,24 +1,93 @@
+using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
-
-public class PlayerAim : MonoBehaviour
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+namespace anstjddn
 {
-    [SerializeField] GameObject aimobj;
-
-
-    private Vector3 aimpos;
-    private void Update()
+    public class PlayerAim : MonoBehaviour
     {
-        aimobj.transform.position = new Vector3(aimpos.x,0,aimpos.z);
-    }
+
+        [SerializeField] GameObject aimobj;   //예시(나중에 삭제)
 
 
-    private void OnPoint(InputValue Value)
-    {
-        aimpos.x = Value.Get<Vector2>().x;
-        aimpos.z = Value.Get<Vector2>().y;
-        aimpos = Camera.main.ScreenToWorldPoint(new Vector3(aimpos.x, 0, aimpos.z));
+        //어택범위,충돌할 레이어, 공을 미는힘
+        [SerializeField] public float attacksize;
+        [SerializeField] LayerMask ball;
+        [SerializeField] public float attackpower;
+        [SerializeField] public float attacktime;
+        [SerializeField]public bool isattack;
+
+
+       // [SerializeField] Image attackimage;
+      //  [SerializeField] GameObject hideImage;
+
+
+       [SerializeField] private UnityEvent Attacksound;  //나중에 어택 사운드
+
+        private Vector3 mousepos;
+
+        private void Start()
+        {
+            isattack = false;
+        }
+
+        private void Update()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                mousepos = hit.point;
+                mousepos.y = 0;
+            }
+            Debug.Log(mousepos);
+            //마우스 포인터 제대로 인식하는지 예시 나중에 삭제
+            aimobj.transform.position = mousepos;
+        }
+        private void OnAttack(InputValue Value)
+        {
+
+            Attack();
+        }
+
+        private void Attack()
+        {
+            StartCoroutine(AttackTimeing(attacktime));
+        }
+
+
+        // 어택 범위 설정
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, attacksize);
+        }
+
+        //어택타이밍 구현
+        IEnumerator AttackTimeing(float attacktime)
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, attacksize, ball);
+            foreach (Collider collider in colliders)
+            {
+                if (isattack == false && collider.gameObject.layer == 7)                //레이어 7번이 ball로 설정
+                {
+
+                    isattack = true;
+                    Vector3 dir = (mousepos - transform.position).normalized;
+                   
+                    collider.GetComponent<Rigidbody>().velocity = dir * attackpower;
+                    Attacksound?.Invoke();
+                    yield return new WaitForSeconds(attacktime);
+                    isattack = false;
+                }
+                //플레이어 공격 구현 필요
+               
+            }
+        }
     }
 }
--- Assets/Game/anstjddn/playercontroll.cs
+++ Assets/Game/anstjddn/playercontroll.cs
@@ -5,72 +5,41 @@ using Unity.VisualScripting;
 using UnityEditor.AnimatedValues;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-public class playercontroll : MonoBehaviour
+namespace anstjddn
 {
 
-    //플레이어 무브 관련
-    private Rigidbody playerrb;
-    private Vector3 movedir;
-    [SerializeField] private float movespeed;
 
-
-    //플레이어 어택관련
-    [SerializeField] float attacksize;
-    [SerializeField] LayerMask ball;
-    [SerializeField] float attackpower;
-         private Vector3 aimpos;
-    [SerializeField] Camera main;
-
-    private void Awake()
+    public class playercontroll : MonoBehaviour
     {
-        playerrb = GetComponent<Rigidbody>();  
-    }
 
-    private void Update()
-    {
-        Move();
- 
-    }
-    private void Move()
-    {
-        // transform.Translate(-movedir.x * movespeed * Time.deltaTime, 0, -movedir.z * movespeed * Time.deltaTime);
-        playerrb.velocity = new Vector3(-movedir.x * movespeed, 0, -movedir.z * movespeed);
+        //플레이어 무브 관련
+        private Rigidbody playerrb;
+        private Vector3 movedir;
+        [SerializeField] private float movespeed;
 
-    }
 
-    private void OnMove(InputValue Value)
-    {
-        movedir.x = Value.Get<Vector2>().x;
-        movedir.z = Value.Get<Vector2>().y;
-    }
-    private void OnAttack(InputValue Value)
-    {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, attacksize, ball);
-         foreach (Collider collider in colliders)
+        private void Awake()
         {
-            
-            Vector3 dir = transform.position - collider.transform.position;
-            collider.GetComponent<Rigidbody>().velocity = dir * -attackpower;
-
-
-            //플레이어 공격 구현 필요
+            playerrb = GetComponent<Rigidbody>();
         }
-        Debug.Log("좌클릭");
-    }
 
-    private void OnPoint(InputValue Value)
-    {
-        aimpos.x = Value.Get<Vector2>().x;
-        aimpos.z = Value.Get<Vector2>().y;
-      
-    }
+        private void Update()
+        {
+            Move();
 
+        }
+        private void Move()
+        {
 
-    // 어택 범위 설정
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, attacksize);
+            playerrb.velocity = new Vector3(-movedir.x * movespeed, 0, -movedir.z * movespeed);
+
+        }
+
+        private void OnMove(InputValue Value)
+        {
+            movedir.x = Value.Get<Vector2>().x;
+            movedir.z = Value.Get<Vector2>().y;
+        }
+       
     }
 }
