7c0f3aa0ca2c77bd9ba8622abe4b53ca6edb09f4
Eo jin <ddw4500@naver.com>
2023-08-14 16:02:47 +0900

    feat: the real complete draw skill effect
    edit skill has more effects, and disappear time by effect move speed

--- Assets/Game/Scripts/EoJin/Player/DrawSkillEffect.cs
+++ Assets/Game/Scripts/EoJin/Player/DrawSkillEffect.cs
@@ -23,11 +23,11 @@ public class DrawSkillEffect : MonoBehaviour
     {
         if (effects.Count > 0)
         {
-            Vector3 playerNInst = startPos - effects[5].transform.position;
+            Vector3 playerNInst = startPos - effects[10].transform.position;
             Vector3 instVecButYIsZero = new Vector3(playerNInst.x, 0f, playerNInst.z);
             
-            //0.2f더한 이유는 너무 바로 사라져서
-            if (Mathf.Abs(instVecButYIsZero.x) > Mathf.Abs(destination.x) + 0.2f || Mathf.Abs(instVecButYIsZero.z) > Mathf.Abs(destination.z) + 0.2f)
+            //2.5f더한 이유는 조금 오바되는 게 시각적으로 좋을 것 같아서 이펙트 속도 0.3기준
+            if (Mathf.Abs(instVecButYIsZero.x) > Mathf.Abs(destination.x) + 2.5f || Mathf.Abs(instVecButYIsZero.z) > Mathf.Abs(destination.z) + 2.5f)
                 DestroyAllEffects();
         }
     }
@@ -61,9 +61,9 @@ public class DrawSkillEffect : MonoBehaviour
         Vector3 dir = (transform.position - aim.mousepos);
         cubeForLookAt.transform.rotation = Quaternion.LookRotation(transform.position - aim.mousepos);
 
-        for (int i = -5; i <= 5; i++)
+        for (int i = -10; i <= 10; i++)
         {
-            float angle = skillAttacker.skill.angle * (0.2f * i);
+            float angle = skillAttacker.skill.angle * (0.1f * i);
             GameObject instance = Instantiate(skillAttacker.skill.effectPrefab, transform.position, cubeForLookAt.transform.rotation * Quaternion.Euler(0f, angle, 0f));
             
             effects.Add(instance);
