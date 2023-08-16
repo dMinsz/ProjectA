2d9c47803920f0ef3c42f7d5253cdbd9b1dead06
rpoiu0320 <129036265+rpoiu0320@users.noreply.github.com>
2023-08-16 19:05:12 +0900

    feat: InputFiled Case Check When SingnUp
    회원가입할 때 여러 조건들 추가

--- Assets/Network/Scripts/SignUpPanel.cs
+++ Assets/Network/Scripts/SignUpPanel.cs
@@ -2,8 +2,10 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class SignUpPanel : MonoBehaviour
 {
@@ -14,12 +16,54 @@ public class SignUpPanel : MonoBehaviour
     [SerializeField] LobbyManager Lm;
 
     bool canUsedId = false;
+
     private void OnEnable()
     {
         if (DB.isConnect == false)
         {
             DB.Connect();
         }
+
+        idInputField.text = "";
+        nameInputField.text = "";
+        PWInputField.text = "";
+    }
+
+    private bool InputFiledCaseCheck(TMP_InputField inputField)
+    {
+        string fieldName = inputField.name.Substring(0, inputField.name.LastIndexOf("InputField"));
+
+        if (inputField.text == null || inputField.text.Count() <= 0)
+        {
+            StatePanel.Instance.AddMessage($"Please Check {fieldName}");
+
+            if (!DB.reader.IsClosed)
+                DB.reader.Close();
+
+            return false;
+        }
+
+        if (inputField.text.Contains(" "))
+        {
+            StatePanel.Instance.AddMessage($"{fieldName} is contains spaces");
+
+            if (!DB.reader.IsClosed)
+                DB.reader.Close();
+
+            return false;
+        }
+
+        if (inputField.text.Count() <= 2)       // 3글자 이상만 가능
+        {
+            StatePanel.Instance.AddMessage($"Enter {fieldName} of 3 characters or more");
+
+            if (!DB.reader.IsClosed)
+                DB.reader.Close();
+
+            return false;
+        }
+
+        return true;
     }
 
     public void CheckID() 
@@ -27,9 +71,11 @@ public class SignUpPanel : MonoBehaviour
         try 
         {
             string id = idInputField.text;
-            if (id == null) 
+
+            if (!InputFiledCaseCheck(idInputField))
             {
-                StatePanel.Instance.AddMessage($"CheckID is null error :");
+                canUsedId = false;
+                return;
             }
 
             string sqlCommand = string.Format("SELECT id FROM user_info WHERE id='{0}'", id);
@@ -56,7 +102,7 @@ public class SignUpPanel : MonoBehaviour
         }
         catch (Exception ex)
         {
-            StatePanel.Instance.AddMessage($"SignUp CheckID error : {ex.Message}");
+            //StatePanel.Instance.AddMessage($"SignUp CheckID error : {ex.Message}");
             Debug.Log(ex.Message);
         }
     }
@@ -65,13 +111,15 @@ public class SignUpPanel : MonoBehaviour
     {
         try
         {
-
             if (canUsedId == false) 
             {
                 Debug.Log("Please check id duplicate");
                 StatePanel.Instance.AddMessage("Please check id duplicate");
                 return;
             }
+
+            if (!InputFiledCaseCheck(nameInputField) || !InputFiledCaseCheck(PWInputField))
+                return;
 
             string id = idInputField.text;
             string name = nameInputField.text;
@@ -80,10 +128,9 @@ public class SignUpPanel : MonoBehaviour
             string sqlCommand = string.Format("INSERT INTO user_info (id,nickname,password) VALUES ( '{0}','{1}','{2}')", id , name, pass);
             MySqlCommand cmd = new MySqlCommand(sqlCommand, DB.connection);
 
-
             if (cmd.ExecuteNonQuery() == 1)
             {
-                StatePanel.Instance.AddMessage(name + "Member registration complete, id : " + id);
+                StatePanel.Instance.AddMessage($"{name} Member registration complete, id : {id}");
                 if (!DB.reader.IsClosed)
                     DB.reader.Close();
             }
